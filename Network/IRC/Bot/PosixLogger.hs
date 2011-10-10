118b1eade03da6b9e0ff82c3ce7af58dde2e94a2
Jeremy Shaw <jeremy@seereason.com>
2011-10-10 20:30:49 +0000

    0.1.0 - exposed PosixLogger, debianized

--- Network/IRC/Bot/PosixLogger.hs
+++ Network/IRC/Bot/PosixLogger.hs
@@ -1,21 +1,20 @@
 module Network.IRC.Bot.PosixLogger where
 
-import Commands
 import Control.Concurrent.Chan
 import Data.Time.Calendar (Day(..))
 import Data.Time.Clock (UTCTime(..), addUTCTime, getCurrentTime)
 import Data.Time.Format (formatTime)
 import Network.IRC (Command, Message(Message, msg_prefix, msg_command, msg_params), Prefix(NickName), UserName, encode, decode, joinChan, nick, user)
+import Network.IRC.Bot.Commands
 import System.FilePath
 import System.Locale
-import System.Log.Logger (Priority(DEBUG, INFO, ERROR), logM, rootLoggerName, setHandlers, setLevel, updateGlobalLogger)
 import System.Posix
 
 -- TODO: This should be modified so that a formatting filter can be applied to the log messages
 -- TODO: should be updated so that log file name matches channel
 -- TODO: should support multiple channels
-logger :: Maybe FilePath -> String -> Chan Message -> IO ()
-logger mLogDir channel logChan =
+posixLogger :: Maybe FilePath -> String -> Chan Message -> IO ()
+posixLogger mLogDir channel logChan =
   do now <- getCurrentTime
      let logDay = utctDay now
      logFd <- openLog now
@@ -26,7 +25,7 @@ logger mLogDir channel logChan =
           case mLogDir of
             Nothing -> return Nothing
             (Just logDir) ->
-                do let logPath = logDir </> (formatTime defaultTimeLocale "happs-%Y-%m-%d.txt" now)
+                do let logPath = logDir </> (formatTime defaultTimeLocale ((dropWhile (== '#') channel) ++ "-%Y-%m-%d.txt") now)
                    fd <- openFd logPath WriteOnly (Just 0o0644) (defaultFileFlags { append = True })
                    return (Just fd)
       updateLogHandle :: UTCTime -> Day -> Maybe Fd -> IO (Day, Maybe Fd)
@@ -46,11 +45,10 @@ logger mLogDir channel logChan =
            case mPrivMsg of
              (Just (PrivMsg (Just (NickName nick _user _server)) receivers msg)) | channel `elem` receivers ->
                    do let logMsg = showString (formatTime defaultTimeLocale "%X " now) . showString "<" . showString nick . showString "> " $ msg
-                      logM "irc" INFO logMsg
                       case mLogFd' of
                         Nothing -> return ()
                         (Just logFd') -> fdWrite logFd' (logMsg ++ "\n") >> return ()
                       return ()
                       -- hPutStrLn logFd logMsg
-             _ -> logM "irc" INFO (show msg)
+             _ -> return ()
            logLoop logDay' mLogFd'
