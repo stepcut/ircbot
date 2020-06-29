2c149434a03b839b9770d0267dfe71c03a0e412c
Richard Marko <srk@48.io>
2020-06-29 19:49:31 +0200

    Simplify shell.nix using callCabal2nix, move to default.nix

--- default.nix
+++ default.nix
@@ -1,16 +1,5 @@
-{ mkDerivation, base, bytestring, containers, directory, filepath
-, irc, mtl, network, old-locale, parsec, random, SafeSemaphore
-, stdenv, stm, time, unix
-}:
-mkDerivation {
-  pname = "ircbot";
-  version = "0.6.3";
-  src = ./.;
-  buildDepends = [
-    base bytestring containers directory filepath irc mtl network
-    old-locale parsec random SafeSemaphore stm time unix
-  ];
-  homepage = "http://hub.darcs.net/stepcut/ircbot";
-  description = "A library for writing irc bots";
-  license = stdenv.lib.licenses.bsd3;
-}
+{ pkgs ? import <nixpkgs> {} }:
+let
+  drv = pkgs.haskellPackages.callCabal2nix "ircbot" ./. {};
+in
+  if pkgs.lib.inNixShell then drv.env else drv
